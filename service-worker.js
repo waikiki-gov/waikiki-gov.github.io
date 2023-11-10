841a62d4315ce806363346c7cb660f7a0b5275e1
kristof <pappkristof1@gmail.com>
2023-11-10 20:44:28 +0100

    Service worker

--- service-worker.js
+++ service-worker.js
@@ -4,8 +4,13 @@ self.addEventListener("fetch", (e) => {
             const cache = await caches.open("WaikikiWebsiteCache");
             let response = await cache.match(e.request);
             if (!response) {
-                response = await fetch(e.request);
-                cache.put(e.request, response.clone());
+                try {
+                    response = await fetch(e.request);
+                    cache.put(e.request, response.clone());
+                    console.log("Loaded from network: " + e.request.url);
+                } catch (ex) {
+                    console.log("Error during request: " + e.request.url);
+                }
             }
             return response;
         })());
